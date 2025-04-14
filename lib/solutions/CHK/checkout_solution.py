4135237cd1244f45045a6a92c3c713853bc7166a
sbx_user1051 <sbx_user1051@169.254.66.53>
2025-04-14 12:26:08 +0000

    Mon Apr 14 12:26:08 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,17 @@
 ERROR_CODE = -1
 
+PRICES = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+}
+
+OFFERS = {
+    "A": (3, 130),  # 3 A's for 130
+    "B": (2, 45),   # 2 B's for 45
+}
+
 
 class CheckoutSolution:
 
@@ -14,6 +26,7 @@ class CheckoutSolution:
     def checkout(self, skus: list[str]) -> int:
         if not self._validate_skus(skus):
             return ERROR_CODE
+
 
 
 
