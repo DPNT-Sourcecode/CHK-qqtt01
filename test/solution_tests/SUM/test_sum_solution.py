1acacc70fcefa9ecdf39a440c18234a4a3b65163
sbx_user1051 <sbx_user1051@169.254.66.53>
2025-04-14 11:43:28 +0000

    Mon Apr 14 11:43:28 UTC 2025

--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -4,3 +4,10 @@ from solutions.SUM.sum_solution import SumSolution
 class TestSum():
     def test_sum(self):
         assert SumSolution().compute(1, 2) == 3
+
+    def test_non_a_number(self):
+        try:
+            SumSolution().compute(1, "a")
+        except TypeError:
+            assert True
+
