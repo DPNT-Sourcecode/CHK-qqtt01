4977e2ea0b442197e788a590e15b861801091de2
sbx_user1051 <sbx_user1051@169.254.66.53>
2025-04-14 12:43:41 +0000

    Mon Apr 14 12:43:41 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -4,24 +4,25 @@ class TestCheckoutSolution:
     def test_invalid_input(self):
         assert CheckoutSolution().checkout("") == -1
         assert CheckoutSolution().checkout("123") == -1
-        assert CheckoutSolution().checkout([]) == -1
+        assert CheckoutSolution().checkout(None) == -1
 
     def test_checkout_each(self):
-        assert CheckoutSolution().checkout(["A"]) == 50
-        assert CheckoutSolution().checkout(["B"]) == 30
-        assert CheckoutSolution().checkout(["C"]) == 20
-        assert CheckoutSolution().checkout(["D"]) == 15
+        assert CheckoutSolution().checkout("A") == 50
+        assert CheckoutSolution().checkout("B") == 30
+        assert CheckoutSolution().checkout("C") == 20
+        assert CheckoutSolution().checkout("D") == 15
 
     def test_multiple_product(self):
-        assert CheckoutSolution().checkout(["A"] * 2) == 100
-        assert CheckoutSolution().checkout(["A", "B", "C", "D"]) == 50 + 30 + 20 + 15
+        assert CheckoutSolution().checkout("AA") == 100
+        assert CheckoutSolution().checkout("ABCD") == 50 + 30 + 20 + 15
 
     def test_discounts(self):
-        assert CheckoutSolution().checkout(["A", "A", "A"]) == 130
-        assert CheckoutSolution().checkout(["A", "A", "A"] + ["A"] * 3) == 130 + 130
-        assert CheckoutSolution().checkout(["B", "B"]) == 45
-        assert CheckoutSolution().checkout(["B"] * 4) == 45 * 2
+        assert CheckoutSolution().checkout("AAA") == 130
+        assert CheckoutSolution().checkout("AAAAAA") == 130 + 130
+        assert CheckoutSolution().checkout("BB") == 45
+        assert CheckoutSolution().checkout("BBBB") == 45 * 2
 
     def test_product_have_no_discount(self):
-        assert CheckoutSolution().checkout(["C", "C"]) == 40
-        assert CheckoutSolution().checkout(["D", "D"]) == 30
+        assert CheckoutSolution().checkout("CC") == 40
+        assert CheckoutSolution().checkout("DD") == 30
+
