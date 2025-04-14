99a382698a0a1825fea4e83992d70b4f12ac9ea0
sbx_user1051 <sbx_user1051@169.254.66.53>
2025-04-14 11:50:57 +0000

    Mon Apr 14 11:50:57 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,6 +2,9 @@
 class HelloSolution:
     
     # friend_name = unicode string
-    def hello(self, friend_name: str) -> str:
+    def hello(self, friend_name: str | None = None) -> str:
+        if friend_name is None:
+            return "Hi there!"
         return f"Hello {friend_name}!"
+
 
--- test/solution_tests/HLO/test_hlo_solution.py
+++ test/solution_tests/HLO/test_hlo_solution.py
@@ -4,3 +4,7 @@ from solutions.HLO.hello_solution import HelloSolution
 class TestHello():
     def test_hello(self):
         assert HelloSolution().hello("World") == "Hello World!"
+
+    def test_hello_empty(self):
+        assert HelloSolution().hello("") == "Hi there!"
+
