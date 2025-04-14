7b1c0169ed7a903bc771a38870088cb16b052538
sbx_user1051 <sbx_user1051@169.254.66.53>
2025-04-14 12:00:00 +0000

    Mon Apr 14 12:00:00 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -5,9 +5,6 @@ class HelloSolution:
     
     # friend_name = unicode string
     def hello(self, friend_name: str | None = None) -> str:
-        if friend_name is not None:
-            friend_name = friend_name.strip()
-        if not friend_name:
-            return DEFAULT_MESSAGE
-        return f"Hello {friend_name}!"
+        return DEFAULT_MESSAGE
+
 
