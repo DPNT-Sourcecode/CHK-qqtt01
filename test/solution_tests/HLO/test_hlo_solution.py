8e1d2b5c7ddaf88c3ceb2a7af89ca9225d29c965
sbx_user1051 <sbx_user1051@169.254.66.53>
2025-04-14 12:03:01 +0000

    Mon Apr 14 12:03:01 UTC 2025

--- test/solution_tests/HLO/test_hlo_solution.py
+++ test/solution_tests/HLO/test_hlo_solution.py
@@ -1,20 +1,7 @@
 from solutions.HLO.hello_solution import HelloSolution
-from solutions.HLO.constants import DEFAULT_MESSAGE
 
 class TestHello():
     def test_hello(self):
         assert HelloSolution().hello("World") == "Hello World!"
 
-    def test_hello_empty(self):
-        assert HelloSolution().hello("") == DEFAULT_MESSAGE
-        assert HelloSolution().hello() == DEFAULT_MESSAGE
-        assert HelloSolution().hello("        ") == DEFAULT_MESSAGE
-
-    def test_hello_unicode(self):
-        assert HelloSolution().hello("Иван") == "Hello Иван!"
-        assert HelloSolution().hello("李雷") == "Hello 李雷!"
-        assert HelloSolution().hello("😊") == "Hello 😊!"
-
-    def test_trimmed_name(self):
-        assert HelloSolution().hello("  World  ") == "Hello World!"
 
