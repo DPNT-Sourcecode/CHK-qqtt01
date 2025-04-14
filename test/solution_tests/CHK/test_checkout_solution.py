aa30ef5be3236e88a0820be80b8b550c2a99e924
sbx_user1051 <sbx_user1051@169.254.66.53>
2025-04-14 12:29:08 +0000

    Mon Apr 14 12:29:08 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -5,3 +5,10 @@ class TestCheckoutSolution:
         assert CheckoutSolution().checkout("") == -1
         assert CheckoutSolution().checkout("123") == -1
         assert CheckoutSolution().checkout([]) == -1
+
+    def test_checkout_each(self):
+        assert CheckoutSolution().checkout(["A"]) == 50
+        assert CheckoutSolution().checkout(["B"]) == 30
+        assert CheckoutSolution().checkout(["C"]) == 20
+        assert CheckoutSolution().checkout(["D"]) == 15
+
